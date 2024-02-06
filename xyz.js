8d99e06f658cbced6ffecc34ec1a93801407a6fe
Sourav163 <souravroutray2016@gmail.com>
2024-02-06 23:35:59 +0530

    tries

---
+++ xyz.js
@@ -0,0 +1,10 @@
+// function generateRandomCharacter() {
+//   const characters = ["S", "W", "G"];
+//   const randomIndex = Math.floor(Math.random() * characters.length);
+//   return characters[randomIndex];
+// }
+
+// const randomCharacter = generateRandomCharacter();
+// console.log(randomCharacter);
+
+console.log(Math.random() * 3);
